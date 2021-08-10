ddb8620d4e40765d4582169d5e7f40f837374bfb
Zachary Bryce Ellis <zbellis03@gmail.com>
2021-08-10 15:57:37 -0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,21 @@
+
+name = input("What's your name: ")
+
+
+age = input("What's your age: ")
+
+address = input("What's your address: ")
+
+
+print("Hello {}, you're {} years old, and you live in {}.".format(name, age, address))
+
+
+
+a = input("Enter a number a: ")
+b = input("Enter a number b: ")
+
+int_a = int(a)
+int_b = int(b)
+c = int_a + int_b
+
+print("The sum of {} and {} is {}".format(int_a, int_b, c))
