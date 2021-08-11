87d76529da37561b129fb59764e321b4fabaca60
Zachary Bryce Ellis <zbellis03@gmail.com>
2021-08-11 14:59:50 -0500

    Add files via upload

--- string_manipulations.py
+++ string_manipulations.py
@@ -1,5 +1,19 @@
 
-def extract_email(email, message):
+
+def create_errorMessage():
+    build_error_message1 = input("Copy order number and date: ")
+    serial_number = input("Enter serial number: ")
+
+    full_error_message = ("""I have placed an order for you in the HP website, DCC\n
+    {}.
+    There is no tracking shipment yet, however your toner-cartridge(s) will arrive in 1-2 business days\n
+    When it arrives, please place the toner-cartridges in the device with Serial Number: {}.\n
+    Thanks in advance,""".format(build_error_message1, serial_number))
+
+    return full_error_message
+
+
+def extract_email(email):
 
 
         
@@ -16,22 +30,24 @@ def extract_email(email, message):
     first_name = full_name[0].capitalize()
     last_name = full_name[1].capitalize()
 
-
-
-    if message == 1:        
+    message = create_errorMessage()
+    errorMessageType = input("Enter the type of error message: ")
+    
+    if  errorMessageType == "1":        
         final_message = error_message1
     else:
         final_message = error_message2
 
-    print("Hello {} {}, {}".format(first_name,last_name,final_message))
-
-
-extract_email('zachary.ellis@ipaper.com', 1)
-
-
-
+    print("Hello {} {}, {}. {}".format(first_name,last_name,final_message,message))
     
+
+extract_email('zachary.ellis@ipaper.com')
     
+
+
+
+
+
 
 
 
