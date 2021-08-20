f33eb5ce525094abf36202fddd0f71eca38af556
Zachary Bryce Ellis <zbellis03@gmail.com>
2021-08-20 02:51:59 -0500

    Add files via upload

---
+++ open_cv_drawImage.py
@@ -0,0 +1,12 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as numpy
+import cv2
+
+# put an image in documents
+image_color = mpimg.imread("dumbell.jpg")
+plt.imshow(image_gray, cmap = 'gray')
+image_gray.shape
+print(image_gray)
+
+print('sucess')
