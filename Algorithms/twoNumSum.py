f4b08b7832c0a29aa81df79ed65c107f52aff0cb
Zachary Bryce Ellis <zellis6@hotmail.com>
2021-09-07 03:44:37 -0500

    Create twoNumSum.py
    https://www.algoexpert.io/questions/Two%20Number%20Sum

---
+++ Algorithms/twoNumSum.py
@@ -0,0 +1,20 @@
+def twoSumNum(runs, targetSum):
+    for i in range(len(runs) - 1):
+        firstNum = runs[i]
+
+        for j in range(i + 1, len(runs)):
+            secondNum = runs[j]
+
+            for k in range(j + 1, len(runs)):
+                thirdNum = runs[k]
+                if firstNum + secondNum + thirdNum == targetSum:
+                    return [firstNum, secondNum, thirdNum]
+    return []
+
+runs = [1,2,3,4,5,9,0,56,1,2,3]
+print(twoSumNum(runs, 6))
+
+
+    # [1,2,..n], 18
+    # comparing numbers to equal total
+    # other wise return nil
