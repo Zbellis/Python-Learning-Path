5d47ae2885f7225c889daf7da026f70be591b0e7
Zachary Bryce Ellis <zellis6@hotmail.com>
2021-09-09 11:12:09 -0500

    Update fwdEmails.py

--- Python Automation/fwdEmails.py
+++ Python Automation/fwdEmails.py
@@ -1,15 +1,17 @@
+# This file is modified from the orignal code used at International Paper for confidentiality of the company's B2B partner's email addresses.
+
 # A faster way to copy and paste Email Addresses and send to outlook
 
 import pyperclip
 
-send_to_location = int(input("1 to send to USA or any key for Mexico: "))
+send_to_location = int(input("1 to send to group 1 or any key for group 2: "))
 
 if send_to_location == 1:
-    usa = "ingrid.alvarado@hp.com; angelica.arriaga1@hp.com; suriel.deleon@hp.com; michael.mack@hp.com; george.doukas@hp.com;"
-    pyperclip.copy(usa)
+    group1 = "user1; user2...10;"
+    pyperclip.copy(group1)
     pyperclip.paste()
 else:
-    mexico = "silvana.pulido@hp.com; michael.mack@hp.com"
-    pyperclip.copy(mexico)
+    group2 = "player1; player2...10"
+    pyperclip.copy(group2)
     pyperclip.paste()
 
